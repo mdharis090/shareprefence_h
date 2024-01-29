74f998d0f1cec11ebe42059ca6f1b399d39f83a9
unknown <harisnazeer090@gmail.com>
2024-01-29 10:42:33 +0500

    shared preference

---
+++ android/app/src/main/kotlin/com/example/shareprefence_h/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shareprefence_h
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //intial intial state
+  int _counter = 0;
+  @override
+  void initState() {
+    readstored();
+    super.initState();
+  }
+
+  void readstored() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('COUNTER', _counter);
+  }
+
+  void _incrementCounter() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    _counter = prefs.getInt('COUNTER') ?? 0;
+    _counter++;
+    await prefs.setInt('COUNTER', _counter);
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: GestureDetector(
+        onTap: _incrementCounter,
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(color: Colors.amber),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  '$_counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
